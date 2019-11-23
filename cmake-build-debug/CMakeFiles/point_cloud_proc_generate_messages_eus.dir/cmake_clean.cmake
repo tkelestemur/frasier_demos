9370d69ccf58c4f07e28e84c9ca2a18f285e04eb
Tarik Kelestemur <tkelestemur@gmail.com>
2019-11-22 21:38:20 -0500

    remove cmake

--- cmake-build-debug/CMakeFiles/point_cloud_proc_generate_messages_eus.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/point_cloud_proc_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/catkin_generated/ordered_paths.cmake
+++
@@ -1,1 +0,0 @@
-set(ORDERED_PATHS "/home/tarik/frasier_ws/devel/.private/frasier_openrave/lib;/home/tarik/frasier_ws/devel/.private/point_cloud_proc/lib;/opt/ros/kinetic/lib")
